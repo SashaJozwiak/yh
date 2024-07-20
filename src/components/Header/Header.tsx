601bb18b92149028fc15399b672a131b05ba2ccf
SashaJozwiak <wmdz88@gmail.com>
2024-07-20 13:44:37 +0300

    fix: remove bal

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,4 @@
-import { useEffect, /* useState */ } from 'react';
+import { useEffect, useState, useCallback } from 'react';
 import { useStore } from '../../store/main';
 
 import { TonConnectButton, useTonAddress } from '@tonconnect/ui-react';
@@ -8,18 +8,52 @@ import s from './header.module.css'
 import './header.css'
 
 export const Header: React.FC = () => {
+
+    const [balance, setBalance] = useState<string | null>(null);
+
     const userFriendlyAddress = useTonAddress();
     const rawAddress = useTonAddress(false);
 
     const user = useStore((state) => state.user);
     const addresses = useStore((state) => (
         {
-            userFriendlyAddress: state.user.userFriendlyAddress,
-            rawAddress: state.user.rawAddress
+            userFriendlyAddress: state.user.userFriendlyAddress || '',
+            rawAddress: state.user.rawAddress || ''
         })
     );
     const setUser = useStore((state) => state.setUser);
     const addAddresses = useStore((state) => state.addAddresses);
+
+    const fetchBalance = useCallback(async (rawAddress) => {
+        console.log(`start fetch for ${rawAddress}`)
+        try {
+            const response = await fetch(`https://tonapi.io/v2/accounts/${encodeURIComponent(rawAddress)}`, {
+                headers: {
+                    'accept': 'application/json'
+                }
+            });
+
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+
+            const data = await response.json();
+            console.log('data: ', data)
+            const tonBalance = (data.balance / 10 ** 9).toFixed(2);
+            setBalance(tonBalance);
+        } catch (error) {
+            console.error('Failed to fetch balance:', error);
+        }
+    }, []);
+
+    useEffect(() => {
+
+
+        if (addresses.rawAddress) {
+            fetchBalance(addresses.rawAddress);
+        }
+
+    }, [addresses.rawAddress, fetchBalance]);
 
     useEffect(() => {
         if (rawAddress) {
@@ -64,8 +98,7 @@ export const Header: React.FC = () => {
 
             <div className={s.speed}>
                 <p>1.2/ч.</p>
-                <p>{addresses.rawAddress}</p>
-                <p>{addresses.userFriendlyAddress}</p>
+                <p>{balance}</p>
 
             </div>
 
