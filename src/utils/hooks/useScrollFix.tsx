33735e9fde323e22858839591f16df240161240a
SashaJozwiak <wmdz88@gmail.com>
2024-08-20 15:05:14 +0300

    fix: scroll tests2

--- src/utils/hooks/useScrollFix.tsx
+++ src/utils/hooks/useScrollFix.tsx
@@ -3,70 +3,32 @@ import { useEffect } from 'react';
 const useScrollFix = () => {
     useEffect(() => {
         const overflow = 100;
-        let inputFocused = false;
 
         document.documentElement.classList.add('document');
         document.body.classList.add('body');
 
         const applyStyles = () => {
-            if (!inputFocused) {
-                document.body.style.overflowY = 'hidden';
-                document.body.style.marginTop = `${overflow}px`;
-                document.body.style.height = window.innerHeight + overflow + "px";
-                document.body.style.paddingBottom = `${overflow}px`;
-                window.scrollTo(0, overflow);
-            } else {
-                document.body.style.marginTop = `0px`;
-            }
+            document.body.style.overflowY = 'hidden';
+            document.body.style.marginTop = `${overflow}px`;
+            document.body.style.height = window.innerHeight + overflow + "px";
+            document.body.style.paddingBottom = `${overflow}px`;
+            window.scrollTo(0, overflow);
         };
 
         applyStyles();
 
-        let ts;
-
-        const onTouchStart = (e) => {
-            ts = e.touches[0].clientY;
-        };
-
-        const onTouchMove = (e) => {
-            const scrollableEl = document.querySelector('.scrollable');
-            if (scrollableEl) {
-                const scroll = scrollableEl.scrollTop;
-                const te = e.changedTouches[0].clientY;
-                if (scroll <= 0 && ts < te) {
-                    e.preventDefault();
-                }
+        const handleResize = () => {
+            if (document.activeElement && document.activeElement.tagName === 'INPUT') {
+                document.body.style.marginTop = `0px`;
             } else {
-                e.preventDefault();
+                applyStyles();
             }
         };
 
-        const onFocus = () => {
-            inputFocused = true;
-            applyStyles();
-        };
-
-        const onBlur = () => {
-            inputFocused = false;
-            applyStyles();
-        };
-
-        const input = document.querySelector('input');
-        if (input) {
-            input.addEventListener('focus', onFocus);
-            input.addEventListener('blur', onBlur);
-        }
-
-        document.documentElement.addEventListener('touchstart', onTouchStart, { passive: false });
-        document.documentElement.addEventListener('touchmove', onTouchMove, { passive: false });
+        window.addEventListener('resize', handleResize);
 
         return () => {
-            document.documentElement.removeEventListener('touchstart', onTouchStart);
-            document.documentElement.removeEventListener('touchmove', onTouchMove);
-            if (input) {
-                input.removeEventListener('focus', onFocus);
-                input.removeEventListener('blur', onBlur);
-            }
+            window.removeEventListener('resize', handleResize);
         };
     }, []);
 };
